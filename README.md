efd1c04a4acb6d91deb10ea063841864da35a4f5
sebastian <scarullo@stacks.es>
2022-04-17 19:08:54 +0200

    update setup script

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# k8s-win
+# Kubernetes on Windows
+
+Scripts for setting up a local hybrid Kubernetes cluster, with a Linux master, Linux worker and Windows worker. Currently deploys:
+
+- Docker 19.03.0
+- Kubernetes 1.15.1
+
+> The setup is documented here: [Getting Started with Kubernetes on Windows](https://blog.sixeyed.com/getting-started-with-kubernetes-on-windows/).
+
+## Apps
+
+There are also Kubernetes manifests for some [sample apps](./apps/README.md), running in Windows and Linux pods.
+
+### Credits
+
+The scripts are mostly hacked together from other scripts and docs:
+
+- [Guide for adding Windows Nodes in Kubernetes](https://kubernetes.io/docs/setup/production-environment/windows/user-guide-windows-nodes/)
+
+- [Kubernetes on Windows](https://docs.microsoft.com/en-us/virtualization/windowscontainers/kubernetes/getting-started-kubernetes-windows)
--- setup/ub-1804-setup.sh
+++ setup/ub-1804-setup.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 
-DOCKER_VERSION="5:19.03.0~3-0~ubuntu-bionic"
-KUBERNETES_VERSION="1.15.1-00"
+DOCKER_VERSION="5:20.10.14~3-0~ubuntu-bionic"
+KUBERNETES_VERSION="1.23.5-00"
 
 # turn off swap - for the Kubelet
 swapoff -a 
 sudo sed -ri '/\sswap\s/s/^#?/#/' /etc/fstab
 
-# install Docker 18.09 (https://docs.docker.com/install/linux/docker-ce/ubuntu/)
+# install Docker 20.10 (https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository)
 apt-get update
 apt-get install -y \
-    apt-transport-https \
     ca-certificates \
     curl \
-    gnupg-agent \
-    software-properties-common
+    gnupg \
+    lsb-release
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt-get update
 apt-get install -y \
@@ -29,17 +27,21 @@ apt-get install -y \
     docker-ce-cli=$DOCKER_VERSION \
     containerd.io
 
-# install Kubeadm etc.
-# Note - Bionic packages not available yet
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-deb https://apt.kubernetes.io/ kubernetes-xenial main
-EOF
+# install Kubeadm 1.23.5 
+apt-get update
+apt-get install -y apt-transport-https ca-certificates curl
+curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 apt-get update
 apt-get install -y \
     kubelet=$KUBERNETES_VERSION \
     kubeadm=$KUBERNETES_VERSION \
     kubectl=$KUBERNETES_VERSION
+apt-mark hold kubelet kubeadm kubectl
 
 # set iptables for Flannel
 sysctl net.bridge.bridge-nf-call-iptables=1
+
+# Manage Docker as a non-root user
+usermod -aG docker vagrant
