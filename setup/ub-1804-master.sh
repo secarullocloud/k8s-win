d01d8219f9493043bd207eac0f3990a5aad9773e
sebastian <scarullo@stacks.es>
2022-04-18 08:56:18 +0200

    update setup script

--- setup/ub-1804-master.sh
+++ setup/ub-1804-master.sh
@@ -2,19 +2,21 @@
 
 POD_NETWORK_CIDR="10.244.0.0/16"
 SERVICE_CIDR="10.96.0.0/12"
+KUBERNETES_VERSION="1.23.5"
 
 # initialise Kube
 
 kubeadm init --pod-network-cidr=$POD_NETWORK_CIDR --service-cidr=$SERVICE_CIDR
 
 # install Flannel pod network (https://github.com/flannel-io/flannel)
-# wget https://raw.githubusercontent.com/secarullocloud/k8s-win/main/setup/flannel/kube-flannel-hybrid.yaml
-wget https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
-# wget https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
-
+wget https://raw.githubusercontent.com/secarullocloud/k8s-win/main/setup/flannel/kube-flannel.yml
 
 kubectl --kubeconfig='/etc/kubernetes/admin.conf' apply -f kube-flannel.yml
 
 # check cluster
 export KUBECONFIG=/etc/kubernetes/admin.conf
 kubectl get nodes
+
+# Add Windows Flannel and kube-proxy DaemonSets
+curl -L https://github.com/kubernetes-sigs/sig-windows-tools/releases/latest/download/kube-proxy.yml | sed 's/VERSION/v$KUBERNETES_VERSION/g' | kubectl apply -f -
+kubectl apply -f https://github.com/kubernetes-sigs/sig-windows-tools/releases/latest/download/flannel-overlay.yml
