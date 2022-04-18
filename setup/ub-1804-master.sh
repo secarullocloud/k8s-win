5cacb347e4fe4292bf5819eeaa2c58b04759a693
sebastian <scarullo@stacks.es>
2022-04-18 08:37:21 +0200

    update setup script

--- setup/ub-1804-master.sh
+++ setup/ub-1804-master.sh
@@ -10,6 +10,7 @@ kubeadm init --pod-network-cidr=$POD_NETWORK_CIDR --service-cidr=$SERVICE_CIDR
 # install Flannel pod network (https://github.com/flannel-io/flannel)
 # wget https://raw.githubusercontent.com/secarullocloud/k8s-win/main/setup/flannel/kube-flannel-hybrid.yaml
 wget https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
+# wget https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 
 
 kubectl --kubeconfig='/etc/kubernetes/admin.conf' apply -f kube-flannel.yml
