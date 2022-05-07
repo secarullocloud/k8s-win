3d2e5f42680103a642c483c68ac2031580b489b5
sebastian <scarullo@stacks.es>
2022-05-07 09:25:33 +0200

    update setup script

--- setup/ub-1804-master.sh
+++ setup/ub-1804-master.sh
@@ -17,6 +17,10 @@ kubectl --kubeconfig='/etc/kubernetes/admin.conf' apply -f kube-flannel.yml
 export KUBECONFIG=/etc/kubernetes/admin.conf
 kubectl get nodes
 
+# export config to vagrant host
+# scp -i ./.vagrant/machines/default/virtualbox/private_key -r -P2222 vagrant@127.0.0.1:/home/vagrant/somefolder ./
+
+
 # Add Windows Flannel and kube-proxy DaemonSets
 curl -L https://github.com/kubernetes-sigs/sig-windows-tools/releases/latest/download/kube-proxy.yml | sed 's/VERSION/v$KUBERNETES_VERSION/g' | kubectl apply -f -
 kubectl apply -f https://github.com/kubernetes-sigs/sig-windows-tools/releases/latest/download/flannel-overlay.yml
--- setup/win-2019-setup.ps1
+++ setup/win-2019-setup.ps1
@@ -1,16 +1,17 @@
-$DOCKER_VERSION="20.10.14"
+# Install Docker EE
 
 # disable security :)
-Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -name Shell -Value 'PowerShell.exe -noExit'
-Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
-Uninstall-WindowsFeature Windows-Defender
+# Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -name Shell -Value 'PowerShell.exe -noExit'
+# Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+# Uninstall-WindowsFeature Windows-Defender
 
 # Set the TLS version used by the PowerShell client to TLS 1.2.
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
+# [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
 # install Docker
 Install-WindowsFeature -Name Containers
+Restart-Computer -Force
+$DOCKER_VERSION="20.10.9"
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Install-Module -Name DockerMsftProvider -Repository PSGallery -Force
-Install-Package -Name docker -ProviderName DockerMsftProvider -Force -RequiredVersion $DOCKER_VERSION
-
+Install-Package -Name docker -ProviderName DockerMsftProvider -Force -RequiredVersion $DOCKER_VERSION -Verbose
 Restart-Computer -Force
